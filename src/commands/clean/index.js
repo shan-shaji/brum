caf4b3d9bad0d2e6690083a89d67607e196bc960
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-23 20:08:11 +0530

    feat(command): add clean command initial setup

---
+++ src/commands/clean/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import path from 'path'
+import fs from 'fs'
+import { error, info, warn } from '../../utils/logger'
+
+export const clean = async (folderName) => {
+  const argument = process.argv[4]
+  let isCurrentDir = false
+
+  if (folderName === '.') {
+    isCurrentDir = true
+    folderName = path.basename(process.cwd())
+  }
+
+  if (isCurrentDir) {
+    const files = fs.readdirSync('.')
+    if (files.length == 0) {
+      error('\n Error: No folders found in this directory')
+      process.exit(1)
+    } else if (files.includes('pubspec.yaml')) {
+      // Todo: execute the flutter clean command
+    } else {
+      // Todo: execute the flutter clean command in all files in this folder
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@
 import { program } from './helpers/commander'
 import pkg from '../package.json'
 import { showIntroduction } from './utils/intro'
+import { clean } from './commands/clean/index'
 
 program
   .description(pkg.description)
   .version(pkg.version)
   .usage('<command> [options]')
+
+program
+  .command('clean <folderName>')
+  .description('Deletes the generated files')
+  .action(clean)
 
 program.parse(process.argv)
 
